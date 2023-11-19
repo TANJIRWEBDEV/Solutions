f59fa263d4042873135f5e2650a1118114e9392d
TANJIRWEBDEV <138860006+TANJIRWEBDEV@users.noreply.github.com>
2023-11-19 13:23:48 +0600

    Create aw.ts

---
+++ Array Wrapper/aw.ts
@@ -0,0 +1,50 @@
+class ArrayWrapper {
+  nums: number[];
+  constructor(nums: number[]) {
+    this.nums = nums;
+  }
+
+  valueOf(): number {
+    return this.nums.reduce((prev, cur) => prev + cur, 0);
+  }
+
+  toString(): string {
+    return `[${this.nums}]`;
+  }
+}
+
+const obj1 = new ArrayWrapper([1, 2]);
+const obj2 = new ArrayWrapper([3, 4]);
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
+// 2695. Array Wrapper
+
+// Example 1:
+
+// Input: nums = [[1,2],[3,4]], operation = "Add"
+// Output: 10
+// Explanation:
+// const obj1 = new ArrayWrapper([1,2]);
+// const obj2 = new ArrayWrapper([3,4]);
+// obj1 + obj2; // 10
+// Example 2:
+
+// Input: nums = [[23,98,42,70]], operation = "String"
+// Output: "[23,98,42,70]"
+// Explanation:
+// const obj = new ArrayWrapper([23,98,42,70]);
+// String(obj); // "[23,98,42,70]"
+// Example 3:
+
+// Input: nums = [[],[]], operation = "Add"
+// Output: 0
+// Explanation:
+// const obj1 = new ArrayWrapper([]);
+// const obj2 = new ArrayWrapper([]);
+// obj1 + obj2; // 0
