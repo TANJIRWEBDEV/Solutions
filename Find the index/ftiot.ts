e0b7236bed986421a2d50d139bff5df66b8b6ec4
TANJIRWEBDEV <138860006+TANJIRWEBDEV@users.noreply.github.com>
2023-12-24 12:11:51 +0600

    Create ftiot.ts

---
+++ Find the index/ftiot.ts
@@ -0,0 +1,24 @@
+type str = (haystack: string, needle: string) => number;
+
+var strStr: str = function (haystack, needle) {
+  if (haystack.includes(needle)) {
+    return haystack.indexOf(needle);
+  } else {
+    return -1;
+  }
+};
+const res = strStr("sabutsa", "sad");
+
+// Find the Index of the First Occurrence in a String
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
